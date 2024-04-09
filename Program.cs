fb4de5f190af17284178cb12280e78cf09b8c856
35-9 <35-9@35-9>
2024-04-09 10:00:32 +0300

    123

--- Program.cs
+++ Program.cs
@@ -6,23 +6,24 @@
         {
            int[,] m = new int[5, 5];
            bool Simentrichnost = false;
-             for (int i = 0; i < 5; i++)
-                 for (int j = 0; j < 5; j++)
-                 {
-                     Console.WriteLine("Введите элемент матрицы - a[" + i + "," + j + "]:");
-                     m[i, j] = int.Parse(Console.ReadLine());
-                    
-                    if (m[i, j] != m[j, i])
-                        Simentrichnost = false;
-                     else
+            for (int i = 0; i < 5; i++)
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.WriteLine("Введите элемент матрицы - a[" + i + "," + j + "]:");
+                    m[i, j] = int.Parse(Console.ReadLine());
+
+                    if (m[i, j] == m[j, i])
                         Simentrichnost = true;
-                 }
+                    else
+                        Simentrichnost = false;
+                }
 
 
-             if (Simentrichnost)
-                 Console.WriteLine("Симметрична");
-             else
-                 Console.WriteLine("Не симметрична");
+                    if (Simentrichnost == true)
+                        Console.WriteLine("Симметрична");
+                    else
+                        Console.WriteLine("Не симметрична");
+                
         }
     }
 }
