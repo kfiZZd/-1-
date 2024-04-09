8a799932d5e084aed12d183cd34c794bfddeaad8
35-9 <35-9@35-9>
2024-04-09 09:42:28 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ЛабораторнаяРаб1_Синявин_Ворфоломеев
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int[,] m = new int[5, 5];
+           bool Simentrichnost = false;
+             for (int i = 0; i < 5; i++)
+                 for (int j = 0; j < 5; j++)
+                 {
+                     Console.WriteLine("Введите элемент матрицы - a[" + i + "," + j + "]:");
+                     m[i, j] = int.Parse(Console.ReadLine());
+                    
+                    if (m[i, j] != m[j, i])
+                        Simentrichnost = false;
+                     else
+                        Simentrichnost = true;
+                 }
+
+
+             if (Simentrichnost)
+                 Console.WriteLine("Симметрична");
+             else
+                 Console.WriteLine("Не симметрична");
+        }
+    }
+}
